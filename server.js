df62334c13189c3e27b5a73cd96fee69a806fc67
Psycoga <davidgarrigu@gmail.com>
2025-03-11 14:53:02 -0400

    prueba archivo test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
+    res.send('HELADO DE FRESAAAAAAA');
     });
 
 app.listen(3000, () => {
